7ddbd0fabe9ac85e5e982cc66716f130ca114fc7
HuuLocFIT <nguyenhuuloc7654@gmail.com>
2024-05-31 09:47:15 +0700

    update path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,8 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
 
 const routes: Routes = [
   {
-    path: 'pokemons',
-    component: PokemonsListComponent,
-  },
-  {
     path: '',
-    redirectTo: '/pokemons',
-    pathMatch: 'full',
+    component: PokemonsListComponent,
   },
   {
     path: '**',
